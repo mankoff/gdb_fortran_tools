6e2af20577f4d6243b5254c38e2b4211d7a4117c
aitzkora <marc.fuentes@inria.fr>
2026-09-06 13:00:21 +0200

    use a select case to run separately each case in gdb

--- gdb_fortran_tools/tests/array_type.fpp
+++ gdb_fortran_tools/tests/array_type.fpp
@@ -1,30 +1,36 @@
-! declare 
 #:set TYPES = [('i4' , 'integer(kind=4)' , 'int'  ),&
                ('i8' , 'integer(kind=8)' , 'int'  ),& 
                ('r4' , 'real(kind=4)'    , 'real' ),&
                ('r8' , 'real(kind=8)'    , 'real' ),&
                ('c8' , 'complex(kind=4)' , 'cmplx'),&
                ('c16', 'complex(kind=8)' , 'cmplx')]
-
 #:def dupdim(i)
 #{for j in range(i-1)}#:,#{endfor}#:
 #:enddef
-
 #:set MAX_DIM = 3
-
 program test_array
+ use iso_fortran_env
  implicit none
  integer, parameter :: MAX_DIM = ${MAX_DIM}$
  integer :: i
  integer, parameter :: N  = product([(i,i=1,MAX_DIM)])
+ 
+ character(len=256) :: name_test
+
+ call get_command_argument(1, name_test)
+
+ select case(trim(name_test))
 
 #:for id in range(1,MAX_DIM+1)
   #:for type_name, type_decl, cv in TYPES
-     call test_${type_name}$_${id}$d()
+    case("${type_name}$_${id}$d") 
+        call test_${type_name}$_${id}$d()
   #:endfor
-  call test_logical_${id}$d()
+    case("logical_${id}$d") 
+        call test_logical_${id}$d()
 #:endfor
-
+  case default
+  end select
 contains
 #:for id in range(1,MAX_DIM+1)
    #:for type_name, type_decl, cv in TYPES
@@ -32,7 +38,7 @@ contains
         integer :: i
         ${type_decl}$, allocatable :: a(${dupdim(id)}$)
         a = reshape([(${cv}$(i),i=1,N)],[product([(i,i=max_dim,${id}$,-1)]),(i,i=(${id}$-1),1,-1)])
-        print *, sum(a)
+        print '(a,i0)', "sum = ", nint(real(sum(a)),selected_int_kind(4))
      end subroutine test_${type_name}$_${id}$d
   #:endfor
   subroutine test_logical_${id}$d()
--- gdb_fortran_tools/tests/runtests.py
+++ gdb_fortran_tools/tests/runtests.py
@@ -1,32 +1,68 @@
+#!/usr/bin/env python3
 from subprocess import run
+from os import environ
+import sys
+
+# for pretty-printing
+ok = "✅"
+failed = "❌"
+
+# check for GFT_DIR is well defined
+if not("GFT_DIR" in environ.keys()):
+  print(f"error GFT_DIR is not defined {failed}")
+  sys.exit(-1)
+else :
+  print(f"GFT_DIR variable defined {ok} : {environ["GFT_DIR"]}")
 
 # first let run fypp and gforfran
-run("rm -f array_type.f90 array_type.x".split(" "))
-run("fypp array_type.fpp array_type.f90".split(" "))
-run("gfortran -g array_type.f90 -o array_type.x".split(" "))
+#run("rm -f array_type.f90 array_type.x".split(" "))
+preProcessRet = run("fypp array_type.fpp array_type.f90".split(" "))
+if (preProcessRet.returncode != 0):
+  print(f"Preprocessor phase {failed}")
+  sys.exit(-1)
+else : 
+  print(f"Preprocessing phase      {ok}")
 
-MAX_DIM = 3 #! synchronize in fypp with a sed command
+compileRet = run("gfortran -g array_type.f90 -o array_type.x".split(" "))
+if (compileRet.returncode != 0):
+  print(f"Compile phase       {failed}")
+  sys.exit(-1)
+else:
+  print(f"Compilation phase       {ok}")
+
+# generate a gdb script
+gdbHeader = """set startup-quietly on
+set debuginfod enabled on
+set debuginfod verbose 0
+set debug libthread-db 0
+set auto-load libthread-db on
+set print thread-events off
+set debug threads off
+set interactive-mode off
+set pagination off
+set confirm off
+file array_type.x
+"""         
+                                 
+MAX_DIM = 2 #! synchronize in fypp with a sed command
 for DIM in range(2, MAX_DIM+1):
   for TYPE in ['i4','i8','r4','r8']:
-    filename = f"gdb.scr_{TYPE}_{DIM}"
+    curr = f"{TYPE}_{DIM}d"
+    filename = f"gdb.scr_"+curr
     run(("rm -f "+filename).split(" "))
     with open(filename,"w") as fp:
-        fp.write("set startup-quietly on\n")
-        fp.write("set debuginfod enabled on\n")
-        fp.write("set debuginfod verbose 0\n")
-        fp.write("set debug libthread-db 0\n")
-        fp.write("set debug threads off\n")
-        fp.write("set interactive-mode off\n")
-        fp.write("set print thread-events off\n")
-        fp.write("set pagination off\n")
-        fp.write("set confirm off\n")
-        fp.write("file array_type.x\n")
-        fp.write(f"b test_{TYPE}_{DIM}d\n")
-        fp.write("run\n")
-        fp.write("n\n")
-        fp.write("n\n")
-        fp.write("n\n")
-        fp.write("np sum a\n")
-        fp.write("exit\n")
-    run(("gdb -q -x ./"+filename).split(" "))
-run("rm -f array_type.x array_type.f90 gdb.scr*".split(" "))
+      fp.writelines(gdbHeader)
+      fp.write(f"set args {curr}\n")
+      fp.write(f"b test_{curr}\n")
+      fp.write("run\n")
+      fp.write("n 3\n")
+      fp.write("np sum a\n")
+      fp.write("exit\n")
+    # GDB run the script
+    runRet = run(("gdb -q -x ./"+filename).split(" "))
+    print(f"{curr} -> {(ok if runRet.returncode == 0 else failed) }")
+    # cleaning current file
+    run(["rm", filename])
+      
+# cleaning generated files
+run(["rm", "array_type.x", "array_type.f90"])
