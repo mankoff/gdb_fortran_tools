74322b416203e75a738aa2fe1fa840b95f6210da
Marc fuentes <marc.fuentes@inria.fr>
2026-09-08 10:59:16 +0200

    cosmetical changes to improve conformance to the standard

--- gdb_fortran_tools/tests/README.md
+++ gdb_fortran_tools/tests/README.md
@@ -1,14 +1,64 @@
 # Rationale
-- very first version of testing : test the `np sum array` command for all non-logical types
+- very first version of testing : test the `pycmd array np.sum` for all non-logical types
+
+## Idea
+ - using Fypp we generate a source `array_type.f90` containing a function doing the same job for a list a type and dimensions
+ - we generate in a loop a gdb script which will stop on that function
+ - we use `pycmd` and `sum` operator to test mapping on numpy types
+ - we compare the printed value by the function and the value computed on the numpy array mapped to the fortran tensor
+
 
 # Dependencies
-- gdb
-- gfortran
-- fypp
 
-Let us run `./runtests.py`
+- Gnu GDB
+- gfortran (but maybe could be generalized to `ifx` or `pgf90`)
+- Fypp
+
+# how to use it
+Running
+```bash
+export GFT_DIR=absolute/path/to/gdb_fortran_tools/
+./runtests.py
+```
+must yield 
+```bash
+GFT_DIR variable    ✅ : /path/to/GFT_DIR
+Preprocessing phase ✅
+Compilation phase   ✅
+i4_2d ............. ✅
+i8_2d ............. ✅
+r4_2d ............. ✅
+r8_2d ............. ✅
+c4_2d ............. ✅
+c8_2d ............. ✅
+i4_3d ............. ✅
+i8_3d ............. ✅
+r4_3d ............. ✅
+r8_3d ............. ✅
+c4_3d ............. ✅
+c8_3d ............. ✅
+i4_4d ............. ✅
+i8_4d ............. ✅
+r4_4d ............. ✅
+r8_4d ............. ✅
+c4_4d ............. ✅
+c8_4d ............. ✅
+i4_5d ............. ✅
+i8_5d ............. ✅
+r4_5d ............. ✅
+r8_5d ............. ✅
+c4_5d ............. ✅
+c8_5d ............. ✅
+i4_6d ............. ✅
+i8_6d ............. ✅
+r4_6d ............. ✅
+r8_6d ............. ✅
+c4_6d ............. ✅
+c8_6d ............. ✅
+```
 
 # TODO
- - add test logical
- - compare the value computed by `sum(a)` and `np sum a`
- 
+ - [ ] add test for logical using `np.anykkkk`
+ - [X] compare the value computed by `sum(a)` and `np sum a`
+ - [ ] add the 1d test (problem in fortran)
+ - [ ] replace the ugly loop to insert MAX_DIM value in array_type.fpp
--- gdb_fortran_tools/tests/runtests.py
+++ gdb_fortran_tools/tests/runtests.py
@@ -6,51 +6,54 @@ import re
 import numpy as np
 
 # for pretty-printing
-ok = "✅"
-failed = "❌"
+OK = "✅"
+FAILED = "❌"
 
 # check for GFT_DIR is well defined
-if not("GFT_DIR" in environ.keys()):
-  print(f"error GFT_DIR is not defined {failed}")
-  sys.exit(-1)
-else :
-  print(f"GFT_DIR variable    {ok} : {environ["GFT_DIR"]}")
 
-# put MAX_DIM in FPP
+if "GFT_DIR" in list(environ):
+    print(f"GFT_DIR variable    {OK} : {environ["GFT_DIR"]}")
+else:
+    print(f"error GFT_DIR is not defined {FAILED}")
+    sys.exit(-1)
+
+# insert MAX_DIM value in array_type.fpp
 MAX_DIM = 6
-with open("array_type.fpp","r") as fp_fpp:
-    lines=fp_fpp.readlines()
+with open("array_type.fpp", "r") as fp_fpp:
+    lines = fp_fpp.readlines()
     fp_fpp.close()
     with open("array_type.fpp", "w+") as fp_fp2:
         for s in lines:
-           if re.match(r"^.*set MAX_DIM\s=.*$",s)!=None:
-               sp = f"#:set MAX_DIM = {MAX_DIM}\n"
-           else:
-               sp = s
-           fp_fp2.write(sp)
+            if re.match(r"^.*set MAX_DIM\s=.*$", s) is not None:
+                sp = f"#:set MAX_DIM = {MAX_DIM}\n"
+            else:
+                sp = s
+            fp_fp2.write(sp)
         fp_fp2.close()
-# first let run fypp and gforfran
-preProcessRet = run("fypp array_type.fpp array_type.f90".split(" "))
-if (preProcessRet.returncode != 0):
-  print(f"Preprocessing phase {failed}")
-  sys.exit(-1)
-else : 
-  print(f"Preprocessing phase {ok}")
 
-compileRet = run("gfortran -g array_type.f90 -o array_type.x".split(" "))
-if (compileRet.returncode != 0):
-  print(f"Compile phase       {failed}")
-  sys.exit(-1)
+# first let run fypp and gforfran
+prepro_ret = run(["fypp", "array_type.fpp",
+                  "array_type.f90"], check=False)
+if prepro_ret.returncode != 0:
+    print(f"Preprocessing phase {FAILED}")
+    sys.exit(-1)
 else:
-  print(f"Compilation phase   {ok}")
+    print(f"Preprocessing phase {OK}")
+
+comp_ret = run(["gfortran", "-g", "array_type.f90",
+                "-o", "array_type.x"], check=False)
+if comp_ret.returncode != 0:
+    print(f"Compilation phase   {FAILED}")
+    sys.exit(-1)
+else:
+    print(f"Compilation phase   {OK}")
 
 
 # compute check_val
-check_value  = int(np.sum(np.arange(np.prod(np.arange(1,MAX_DIM+1)))))
-
+check_value = int(np.sum(np.arange(np.prod(np.arange(1, MAX_DIM+1))+1)))
 
 # generate a gdb script
-gdbHeader = """set startup-quietly on
+GDBHEADER = """set startup-quietly on
 set debuginfod enabled on
 set debuginfod verbose 0
 set debug libthread-db 0
@@ -61,27 +64,35 @@ set interactive-mode off
 set pagination off
 set confirm off
 file array_type.x
-"""         
+"""
 
 for DIM in range(2, MAX_DIM+1):
-  for TYPE in ['i4','i8','r4','r8','c4','c8']:
-    curr = f"{TYPE}_{DIM}d"
-    filename = f"gdb.scr_"+curr
-    run(("rm -f "+filename).split(" "))
-    with open(filename,"w") as fp:
-      fp.writelines(gdbHeader)
-      fp.write(f"set args {curr}\n")
-      fp.write(f"b test_{curr}\n")
-      fp.write(f"run\n")
-      fp.write("n 3\n")
-      fp.write("pycmd a np.sum(np.int_(np.real(_)))\n")
-      fp.write("exit\n")
-    # GDB run the script
-    outFile = curr+".out"
-    runRet = run(("gdb -q -x ./"+filename).split(" "), stdout=open(outFile, "w"))
-    print(f"{curr} ............. {(ok if runRet.returncode == 0 else failed) }")
-    #print(int(open(outFile, "r").readlines()[-1].strip()),"==",check_value)
-    # cleaning current file
-    run(["rm", filename, curr+".out"])
+    for TYPE in ['i4', 'i8', 'r4', 'r8', 'c4', 'c8']:
+        curr = f"{TYPE}_{DIM}d"
+        filename = "gdb.scr_"+curr
+        run(["rm", "-f", filename], check=False)
+        with open(filename, "w") as fp:
+            fp.writelines(GDBHEADER)
+            fp.write(f"set args {curr}\n")
+            fp.write(f"b test_{curr}\n")
+            fp.write("run\n")
+            fp.write("n 3\n")
+            fp.write("pycmd a np.sum(np.int_(np.real(_)))\n")
+            fp.write("exit\n")
+        # GDB run the script
+        outFile = curr+".out"
+        with open(outFile, "w") as f1:
+            run_ret = run(["gdb", "-q", "-x", filename], check=False,
+                          stdout=f1)
+            f1.close()
+            with open(outFile, "r") as f2:
+                comp_val = int(f2.readlines()[-1].strip())
+                if (comp_val == check_value) and (run_ret.returncode == 0):
+                    print(f"{curr} ............. {OK}")
+                else:
+                    print(f"{curr} ............. {FAILED}")
+
+        # cleaning current file
+        run(["rm", filename, curr+".out"], check=False)
 # cleaning generated files
-run(["rm", "array_type.x", "array_type.f90"])
+run(["rm", "array_type.x", "array_type.f90"], check=False)
